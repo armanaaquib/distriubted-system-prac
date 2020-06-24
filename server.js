113e37e5458a91e753500f52fc00b9a22c3f5103
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-06-24 08:07:23 +0530

    server-worker

--- server.js
+++ server.js
@@ -1,8 +1,54 @@
 const express = require('express');
-const { processImage } = require('./processImage');
+const http = require('http');
 
 const app = express();
 const PORT = 8000;
+
+let id = 0;
+let isWorkerFree = true;
+const jobs = [];
+
+const getWorkerOptions = (method, path) => {
+  return {
+    host: 'localhost',
+    port: 5000,
+    path,
+    method,
+  };
+};
+
+const delegateToWorker = (id, { count, width, height, tags }) => {
+  const path = `/process/${id}/${count}/${width}/${height}/${tags}`;
+  const options = getWorkerOptions('POST', path);
+
+  const req = http.request(options, (res) => {
+    console.log('Got from worker: ', res.statusCode);
+
+    res.setEncoding('utf8');
+
+    let tags = '';
+
+    res.on('data', (chunk) => {
+      tags += chunk;
+    });
+
+    res.on('data', () => {
+      console.log(tags);
+      isWorkerFree = true;
+    });
+  });
+
+  isWorkerFree = false;
+  req.end();
+};
+
+setInterval(() => {
+  if (isWorkerFree && jobs.length > 0) {
+    const job = jobs.shift();
+    console.log('Scheduling job on worker: ', job.id);
+    delegateToWorker(job.id, job.params);
+  }
+}, 1000);
 
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
@@ -10,10 +56,9 @@ app.use((req, res, next) => {
 });
 
 app.post('/process/:user/:count/:width/:height/:tags', (req, res) => {
-  processImage(req.params).then((tags) => {
-    res.write(JSON.stringify(tags));
-    res.end();
-  });
+  res.end(`id:${id}\n`);
+  jobs.push({ id, params: req.params });
+  id++;
 });
 
 app.listen(PORT, () => console.log(`Server Listening at ${PORT}`));
